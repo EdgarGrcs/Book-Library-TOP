0bfed9351d63fcd5042af21edeb09faa626a47d2
EdgarGrcs <edgar.gricius2@gmail.com>
2022-09-23 16:48:26 +0200

    add partial function to loop through array, needs rework

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-let myLibrary = [];
+// add Function that takes user input on book,
+// and creates an object which it then inserts into array
+// write function that loops through array and displays each book on the page
 
+
+
+
+const div = document.createElement=("div");
+
+
+
+
+let myLibrary = [];
 
 function Book(title,author,numberPages,status){
     this.title = title
@@ -9,7 +20,33 @@ function Book(title,author,numberPages,status){
 }
 
 
-function addBookToLibrary() {
 
-    
+
+
+const book1 = new Book("Deep Work","Cal Newport",221,false);
+
+
+
+function addBookToLibrary(bookObject) {
+myLibrary.push(bookObject);
 }
+
+
+
+function loopArray(array){
+
+    let i = 0;
+    
+    while (i < array.length){
+        array[i].title;
+        array[i].author;
+        array[i].numberPages;
+        array[i].status;
+    }
+
+}
+
+console.log(myLibrary);
+
+
+
