309cc928622b8f2a16d5c61e05a4fb6fcf0d2895
EdgarGrcs <edgar.gricius2@gmail.com>
2022-09-23 13:50:59 +0200

    add index.html, script.js, styles.css

---
+++ script.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+
+function Book(title,author,numberPages,status){
+    this.title = title
+    this.author = author
+    this.numberPages = numberPages
+    this.status = status;
+}
+
+
+function addBookToLibrary() {
+
+    
+}
