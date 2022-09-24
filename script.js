026938b425cdf712b33deeeba383b4c6e37e4967
EdgarGrcs <edgar.gricius2@gmail.com>
2022-09-24 16:25:32 +0200

    add styling to book div

--- script.js
+++ script.js
@@ -3,9 +3,19 @@
 // write function that loops through array and displays each book on the page
 
 
+
+
+/**
+ * To Do:
+ * Implement modal that uses forms to take in user input and store it in a book object
+ * implement a function that removes a book by the press of a button
+ * Implement a function that changes the status of the book from "read" to "not read"
+ */
+
 const book1 = new Book("Deep Work","Cal Newport",221,false);
 const book2 = new Book("Models","Mark Manson",190,true);
 const book3 = new Book("Total recall","Arnold Schwarzenegger",455,true);
+
 
 const myLibrary = [];
 
@@ -22,12 +32,13 @@ myLibrary.push(bookObject);
 }
 
 
+// This function loops through an array which stores book objects and
+// binds them to the DOM by iterating over the array with a while loop
 
 function loopArray(array){
 
-    const ul = document.querySelector(".book-shelf"); 
-    const statusButton = document.querySelector(".status");
-    const removeButton = document.querySelector(".remove-button")
+    const bookShelfDiv = document.querySelector(".book-shelf"); 
+
 
    
     let i = 0;
@@ -46,7 +57,7 @@ function loopArray(array){
         titleDiv.classList.add("title");
         authorDiv.classList.add("author");
         numPagesDiv.classList.add("numPages");
-        statusDiv.classList.add("status");
+        statusDiv.classList.add("read-status");
         readButton.classList.add("status");
         removeButton.classList.add("remove-button");
 
@@ -65,12 +76,11 @@ function loopArray(array){
         div.appendChild(statusDiv);
         div.appendChild(readButton);
         div.appendChild(removeButton);
-        ul.appendChild(div);
+        bookShelfDiv.appendChild(div);
       i++;
     }
 
 }
-
 
 addBookToLibrary(book1);
 addBookToLibrary(book2);
