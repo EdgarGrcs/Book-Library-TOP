b68f9c19375a6554f67e3651bb2665a5e63b5c3f
EdgarGrcs <edgar.gricius2@gmail.com>
2022-09-27 18:18:58 +0200

    change styling of website

--- script.js
+++ script.js
@@ -7,8 +7,6 @@
  */
 
 const book1 = new Book("Deep Work","Cal Newport",221,false);
-const book2 = new Book("Models","Mark Manson",190,true);
-const book3 = new Book("Total recall","Arnold Schwarzenegger",455,true);
 
 let myLibrary = [];
 
@@ -19,26 +17,24 @@ function Book(title,author,numberPages,status){
     this.status = status;
 }
 
-
-
-
 function addBookToLibrary(bookObject) {
     myLibrary.push(bookObject);
  }
 
 
-
+function removeBook(title){
+  myLibrary = myLibrary.filter((book) => book.title !== title )
+  console.log(myLibrary);
+  loopArray(myLibrary);
+}
 
 
 // This function loops through an array which stores book objects and
 // binds them to the DOM by iterating over the array with a while loop
 
-function loopArray(array){
+function addBookCard(book){
 
     const bookShelfDiv = document.querySelector(".book-shelf"); 
-    let i = 0;
-    while (i < array.length){
-       
         const div = document.createElement("div");
         const titleDiv = document.createElement("div");
         const authorDiv = document.createElement("div");
@@ -56,10 +52,10 @@ function loopArray(array){
         removeButton.classList.add("remove-button");
         removeButton.onclick = removeBook;
 
-        titleDiv.textContent = array[i].title;
-        authorDiv.textContent = array[i].author;
-        numPagesDiv.textContent = array[i].numberPages;
-        statusDiv.textContent = (array[i].status === false ? "not read" : "read");
+        titleDiv.textContent = `"${book.title}"`;
+        authorDiv.textContent = book.author;
+        numPagesDiv.textContent = book.numberPages;
+        statusDiv.textContent = (book.status === false ? "not read" : "read");
         readButton.textContent = "READ/UNREAD";
         removeButton.textContent = "Remove";
      
@@ -70,17 +66,16 @@ function loopArray(array){
         div.appendChild(readButton);
         div.appendChild(removeButton);
         bookShelfDiv.appendChild(div);
-      i++;
-    }
 
 }
 
 
-function removeBook(title){
-    myLibrary = myLibrary.filter( (title) => title.title !== title );
-    loopArray(myLibrary);
-}
+function loopArray(){
 
+  for (let book of myLibrary){
+    addBookCard(book);
+  }
+}
 
 
 // Get the modal
@@ -110,33 +105,9 @@ window.onclick = function(event) {
 }
 
 
-const form = document.querySelector("form");
 
-
-form.onclick = function(){
-    
-
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value
-    const numPages = document.getElementById("numPages").value
-    const status = document.getElementById("status");
-
-    const book = new Book(title,author,numPages,status.checked ? true : false);
-    modal.style.display = "none";
-    myLibrary.push(book);
-    loopArray(myLibrary);
-
-}
 
 
 
 addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary(book3);
-
-
 loopArray(myLibrary);
-
-
-
-
