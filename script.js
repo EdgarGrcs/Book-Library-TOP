762865442387727ce91798549378808ae72b4336
EdgarGrcs <edgar.gricius2@gmail.com>
2022-09-30 20:48:30 +0200

    add function that accepts url to add image

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 /**
  * To Do:
- * Implement modal that uses forms to take in user input and store it in a book object
- * implement a function that removes a book by the press of a button
- * Implement a function that changes the status of the book from "read" to "not read"
+ * Understand the remove function and how the id of the book is connected to the DOM
+ * Remove vertical gap between Book divs when there are multiple rows of books
+ * Align checkmark with read status in the modal
  */
 
 const book1 = new Book("Deep Work","Cal Newport",221,false);
@@ -11,11 +11,12 @@ const book2 = new Book("Total Recall","Cal Newport",221,false);
 
 let myLibrary = [];
 
-function Book(title,author,numberPages,status){
+function Book(title,author,numberPages,status,url){
     this.title = title
     this.author = author
     this.numberPages = numberPages
     this.status = status;
+    this.url = url;
 }
 
 function addBookToLibrary(bookObject) {
@@ -40,41 +41,85 @@ submitButton.addEventListener("click",addBook);
 function addBookCard(book){
 
     const bookShelfDiv = document.querySelector(".book-shelf"); 
-        const div = document.createElement("div");
-        const titleDiv = document.createElement("div");
-        const authorDiv = document.createElement("div");
-        const numPagesDiv = document.createElement("div");
-        const statusDiv = document.createElement("div");
-        const readButton = document.createElement("button");
-        const removeButton = document.createElement("button");
-        
-        div.classList.add("book-block");
-        div.setAttribute("id",myLibrary.indexOf(book));
-        titleDiv.classList.add("title");
-        authorDiv.classList.add("author");
-        numPagesDiv.classList.add("numPages");
-        statusDiv.classList.add("read-status");
-        readButton.classList.add("status");
-        removeButton.classList.add("remove-button");
-        removeButton.addEventListener("click",() => {
-          myLibrary.splice(myLibrary.indexOf(book),1);
-          loopArray(myLibrary);
+    const div = document.createElement("div");
+    const titleDiv = document.createElement("div");
+    const authorDiv = document.createElement("div");
+    const numPagesDiv = document.createElement("div");
+    const statusDiv = document.createElement("div");
+    const readButton = document.createElement("button");
+    const removeButton = document.createElement("button");
+    
+    const bookBlockLeft = document.createElement("div");
+    const bookBlockRight = document.createElement("div");
+    const imageDiv = document.createElement("img");
+
+
+    div.classList.add("book-block");
+
+    
+    bookBlockLeft.classList.add("book-block-left");
+    imageDiv.classList.add("imageDiv");
+    bookBlockRight.classList.add("book-block-right");
+
+    div.setAttribute("id",myLibrary.indexOf(book));
+    titleDiv.classList.add("title");
+    authorDiv.classList.add("author");
+    numPagesDiv.classList.add("numPages");
+    
+    if (book.status){
+      statusDiv.classList.add("read-status");
+    } else {
+      statusDiv.classList.add("notRead-status");
+    }
+
+   
+    readButton.classList.add("statusButton");
+    removeButton.classList.add("remove-button");
+    removeButton.addEventListener("click",() => {
+      myLibrary.splice(myLibrary.indexOf(book),1);
+      loopArray(myLibrary);
         });
 
-        titleDiv.textContent = `${book.title}`;
-        authorDiv.textContent = book.author;
-        numPagesDiv.textContent = book.numberPages;
-        statusDiv.textContent = (book.status === false ? "not read" : "read");
-        readButton.textContent = "READ/UNREAD";
-        removeButton.textContent = "Remove";
+    titleDiv.textContent = `${book.title}`;
+    authorDiv.textContent = book.author;
+    numPagesDiv.textContent = book.numberPages;
+    statusDiv.textContent = (book.status === false ? "not read" : "read");
+    readButton.textContent = "READ/UNREAD";
+    removeButton.textContent = "Remove";
+    imageDiv.setAttribute("src",book.url);
+
+    readButton.addEventListener("click",() => {
+      book.status = !book.status;
+      loopArray();
+    })
+
+    bookBlockLeft.appendChild(imageDiv);
+    bookBlockRight.appendChild(titleDiv);
+    bookBlockRight.appendChild(authorDiv);
+    bookBlockRight.appendChild(numPagesDiv);
+    bookBlockRight.appendChild(statusDiv);
+    bookBlockRight.appendChild(readButton);
+    bookBlockRight.appendChild(removeButton);
+    div.appendChild(bookBlockLeft);
+    div.appendChild(bookBlockRight);
+    bookShelfDiv.appendChild(div);
+
+
+
      
-        div.appendChild(titleDiv);
-        div.appendChild(authorDiv);
-        div.appendChild(numPagesDiv);
-        div.appendChild(statusDiv);
-        div.appendChild(readButton);
-        div.appendChild(removeButton);
-        bookShelfDiv.appendChild(div);
+  /*  div.appendChild(titleDiv);
+    div.appendChild(authorDiv);
+    div.appendChild(numPagesDiv);
+    div.appendChild(statusDiv);
+    div.appendChild(readButton);
+    div.appendChild(removeButton);
+    bookShelfDiv.appendChild(div); */ 
+}
+
+function changeReadStatus() {
+
+
+
 
 }
 
@@ -82,21 +127,20 @@ function addBookCard(book){
 function addBook() {
   event.preventDefault();
 
+  const imageUrl = document.querySelector("#imageLink").value;
+  console.log(imageUrl);
+
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
   const numPages = document.querySelector("#numPages").value;
   const status = document.querySelector("#status");
 
-  const book = new Book(title,author,numPages,status.checked ? true : false);
+  const book = new Book(title,author,numPages,status.checked ? true : false,imageUrl);
   addBookToLibrary(book);
   loopArray(myLibrary);
 
 
 }
-
-
-
-
 
 function loopArray(array){
 
@@ -104,13 +148,10 @@ function loopArray(array){
   const bookBlock = document.querySelectorAll(".book-block");
   bookBlock.forEach(book => bookShelf.removeChild(book));
 
-
   for (let book of myLibrary){
     addBookCard(book);
   }
 }
 
-addBookToLibrary(book1);
-addBookToLibrary(book2);
 
 loopArray(myLibrary);
